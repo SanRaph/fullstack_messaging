fc75886069db8290a24508bbfa3f39dd507268a3
SanRaph <raphytex@gmail.com>
2025-04-04 06:30:51 -0700

    add schema

--- models/index.js
+++ models/index.js
@@ -1,1 +1,10 @@
-const mongoose = require('mongoose');
+const { Schema, } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: { type: String, required: true, unique: true},
+        password: { type: String, required: true, unique: true },
+        address: { type: String, required: true, unique: true },
+    }
+);
+
