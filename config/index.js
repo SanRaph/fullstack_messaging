622dd553a627a9a93a54c76df5b96d81f1123e29
SanRaph <raphytex@gmail.com>
2025-04-04 06:23:48 -0700

    required mongoose in models index.js

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const DB_URI = "mongod://localhost/test";
+const DB_URI = "mongodb://127.0.0.1:27017/my_database";
 
 const option = {
     useNewUrlParser: true,
