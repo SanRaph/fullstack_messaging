578ac08b933e79852a2930543d5d892fb825c96c
SanRaph <raphytex@gmail.com>
2025-04-08 08:43:48 -0700

    add cors

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ let usersRouter = require('./api/users');
 let app = express();
 
 app.use(logger('dev'));
-app.use(cors());
+app.use(cors({origin: 'http://localhost:3001'}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
