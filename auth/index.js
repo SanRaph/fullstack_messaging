61536f265f28b140b847dd9a4e3e8d36222507db
SanRaph <raphytex@gmail.com>
2025-04-09 03:16:28 -0700

    verify token in auth

--- auth/index.js
+++ auth/index.js
@@ -1,8 +1,9 @@
 const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'jssjjsjjIFIFI834';
 
 const auth = (req, res, next) => {
     const token = req.headers.Authorization.split('')[1];
-    const verifiedToken()
+    const verifiedToken = jwt.verify(token, JWT_SECRET);
 
     
 };
