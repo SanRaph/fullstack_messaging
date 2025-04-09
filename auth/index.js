ab6df84107ddd476cebe52f1d03d096f1553e0bb
SanRaph <raphytex@gmail.com>
2025-04-09 03:28:08 -0700

    verify token in auth

--- auth/index.js
+++ auth/index.js
@@ -5,8 +5,17 @@ const auth = (req, res, next) => {
     const token = req.headers.Authorization.split('')[1];
     const verifiedToken = jwt.verify(token, JWT_SECRET);
     const userID = verifiedToken._id;
+    req.auth = userID;
 
-    
+    if(req.params.id && req.params.id !== userID){
+        res.status(401);
+        res.json({
+            message: "You must login",
+            status: 'Not OK'
+        });
+    }else{
+        next();
+    }
 };
 
 module.exports = auth;
